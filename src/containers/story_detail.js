82869ef16bb57f7e0b9d394340c781821a901a94
wuhangchen2279 <wuhangchen2279@gmail.com>
2018-08-22 08:31:24 +1000

    change static image name, and put description into stories

--- src/containers/story_detail.js
+++ src/containers/story_detail.js
@@ -8,20 +8,36 @@ class StoryDetail extends Component {
 
     componentDidMount() {
         const {id} = this.props.match.params;
+        var req = require.context('../static/story_output/', false, /.*\.png$/)
+        console.log(req.keys());
     }
 
     render() {
         const {Content} = Layout;
+        const {story} = this.props;
 
-        if(!this.props.story) {
+        if(!story) {
             return <div>Loading</div>
         }
         
         return(
             <Content style={{height: 'calc(100vh - 64px)'}}>
                 <div className="story-container">
-                    <div className="story-info" style={{backgroundColor: 'red'}}>
+                    
+                    <div 
+                        className="story-info" 
+                        style={{backgroundImage: `url(${require(`../static/story_bg/Story${story.id}_bg.png`)})`}}>
+                        <div className="story-desc">
+                            {story.description}
+                        </div>
+                        <div className="story-interaction">
+                            <div className="story-input">
 
+                            </div>
+                            <div className="story-output">
+                                <img width="160px" src={require('../static/story_output/Story1_outputItem_0.png')} />
+                            </div>
+                        </div>
                     </div>
                 </div>
             </Content>
