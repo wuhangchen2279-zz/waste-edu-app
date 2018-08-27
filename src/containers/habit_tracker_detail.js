20283bdd25048431243d4645c16f455d78367fb8
wuhangchen2279 <wuhangchen2279@gmail.com>
2018-08-27 23:06:44 +1000

    fix bug

--- src/containers/habit_tracker_detail.js
+++ src/containers/habit_tracker_detail.js
@@ -1,9 +1,7 @@
 import React, { Component , PropTypes} from 'react';
 import {connect} from 'react-redux';
-import { Layout } from 'antd';
 import { getAllHabits } from '../actions';
 import { Carousel } from 'antd';
-import {PdfExport} from '../components/habit_pdf';
 
 class HabitTrackerDetail extends Component {
 
