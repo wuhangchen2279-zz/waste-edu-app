56e1e4fee4dccd65190bffa2b03f3605b1dad047
wuhangchen2279 <wuhangchen2279@gmail.com>
2018-08-29 22:40:56 +1000

    adjust home page ui

--- src/components/home.js
+++ src/components/home.js
@@ -1,14 +1,33 @@
 import React, { Component } from 'react';
-import { Layout } from 'antd';
-import { Carousel } from 'antd';
-import { Link } from 'react-router-dom';
 import HomeSliders from '../components/home_slider';
+import { Link } from 'react-router-dom';
 
-const Home = (props) => {
+
+const Home = () => {
 
     return (
         <div className="container">
             <HomeSliders animDuration={1} />
+            <div style={{position: "relative", height: "0px"}}>
+            
+                <div style={{
+                    position: "relative",
+                    fontSize: "1.1em",
+                    bottom: "650px"
+                }}>
+                    <span className="mb-4" style={{width: "500px"}}>
+                        <p>Little Angel</p>
+                        <p>Do you know every year more than 130,000 tonnes of plastics end up in the ocean?</p>
+                        <p>These plastics are causing marine animals die</p>
+                        <p>Would you be willing to take actions to save their lives?</p>
+                    </span>
+                    <Link to="/stories"
+                        style={{ width: '200px', marginTop: '5px' }}
+                        className="btn btn-primary btn-xl font-weight-light mb-1">
+                        Read Story Now!
+                    </Link>
+                </div>
+            </div>
         </div>
     );
 }
--- src/components/home_slider.js
+++ src/components/home_slider.js
@@ -1,73 +1,56 @@
 import React, { Component } from 'react';
-import {Link} from 'react-router-dom';
 
 
 class HomeSliders extends React.Component {
-        constructor(props) {
-          super(props)
-        this.backgrounds = [require("../static/home/Home01.png"), 
-          require("../static/home/Home02.png"),
-          require("../static/home/Home03.png"),
-          require("../static/home/Home04.png"),
-          require("../static/home/Home05.png"),
-          require("../static/home/Home06.png"),
-          require("../static/home/Home07.png"),
-          require("../static/home/Home08.png"),
-          require("../static/home/Home09.png"),
-          require("../static/home/Home10.png")]
-        this.state = { backgroundIndex: 0 }
-        
-        this.changeBackground = this.changeBackground.bind(this)
-      }
-    
-      componentDidMount () {
-        this.timeout = setTimeout(
-          this.changeBackground,
-          this.props.animDuration * 1000
-        )
-      }
-      
-      componentWillUnmount() {
-          if (this.timeout) clearTimeout(this.timeout)
-      }
-    
-      changeBackground () {
-        this.setState(function ({ backgroundIndex }) {
-          const nextBackgroundIndex = ++backgroundIndex % this.backgrounds.length
-    
-          return { backgroundIndex: nextBackgroundIndex }
-        }, function () {
-          this.timeout = setTimeout(
-            this.changeBackground,
-            this.props.animDuration * 1000
-          )
-        })
-      }
-    
-      render () {
-        return (
-          <div style={{margin: "5rem auto"}}>
-          <h2 className="mb-4">
-            <p>Little angel</p> 
-            <p>Do you know every year more than 130,000 tonnes of plastics end up in the ocean and cause marine animals decreasing?</p>
-            <p>Would you be willing to take actions to save their lives?</p></h2>
-            <img src={ this.backgrounds[this.state.backgroundIndex] } />
-            <div style={{
-              position: "relative",
-              bottom: "478px"
-            }}>
-              {/* <h2 className="mb-0">Fight Waste for Animals !</h2> */}
-              {/* <hr className="star-light"/> */}
-                <Link to="/stories" 
-                    style={{width: '200px', marginTop: '213px'}}
-                    className="btn btn-primary btn-xl font-weight-light mb-1">
-                    Read Story Now!
-                </Link>
-            </div>
-            
-          </div>
-          
-        )
-      }
-    }
+  constructor(props) {
+    super(props)
+    this.backgrounds = [require("../static/home/Home01.png"),
+    require("../static/home/Home02.png"),
+    require("../static/home/Home03.png"),
+    require("../static/home/Home04.png"),
+    require("../static/home/Home05.png"),
+    require("../static/home/Home06.png"),
+    require("../static/home/Home07.png"),
+    require("../static/home/Home08.png"),
+    require("../static/home/Home09.png"),
+    require("../static/home/Home10.png")]
+    this.state = { backgroundIndex: 0 }
+
+    this.changeBackground = this.changeBackground.bind(this)
+  }
+
+  componentDidMount() {
+    this.timeout = setTimeout(
+      this.changeBackground,
+      this.props.animDuration * 1000
+    )
+  }
+
+  componentWillUnmount() {
+    if (this.timeout) clearTimeout(this.timeout)
+  }
+
+  changeBackground() {
+    this.setState(function ({ backgroundIndex }) {
+      const nextBackgroundIndex = ++backgroundIndex % this.backgrounds.length
+
+      return { backgroundIndex: nextBackgroundIndex }
+    }, function () {
+      this.timeout = setTimeout(
+        this.changeBackground,
+        this.props.animDuration * 1000
+      )
+    })
+  }
+
+  render() {
+    return (
+      <div style={{ margin: "10px auto", display: "flex", justifyContent: 'center' }}>
+        <img height="665px" src={this.backgrounds[this.state.backgroundIndex]} />
+      </div>
+
+
+    )
+  }
+}
 export default HomeSliders;
